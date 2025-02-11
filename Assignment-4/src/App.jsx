ae0cf3d471853e1d498cbd0955f0decf157495e2
mrvigneshgaddam <vigneshgaddam100@gmail.com>
2025-02-11 14:20:25 +0530

    Assignment 4

---
+++ Assignment-4/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import WelcomeMessage from './Components/WelcomeMessage'
+const App = () => {
+  return (
+    <>
+      <WelcomeMessage />
+    </>
+  )
+}
+
+export default App
