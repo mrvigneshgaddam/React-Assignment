c8c267607fce81812087ae2306e483bd059a4450
mrvigneshgaddam <vigneshgaddam100@gmail.com>
2025-02-11 11:39:39 +0530

    Assignment-2 Completed

---
+++ Assignment-2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Greet from './Components/Greet'
+const App = () => {
+  return (
+    <>
+    <Greet />
+    </>
+  )
+}
+
+export default App
---
+++ Assignment-2/src/Components/Greet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Greet = () => {
+  return (
+    <>
+    <h1>Hello, Buddy!</h1>
+    </>
+  )
+}
+
+export default Greet
