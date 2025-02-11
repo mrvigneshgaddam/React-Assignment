eada145afff5a201a2b144568a1d6eeed3c2b377
mrvigneshgaddam <vigneshgaddam100@gmail.com>
2025-02-11 14:30:49 +0530

    Assignment 5

---
+++ Assignment-5/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JSXRules from './Components/JSXRules'
+
+const App = () => {
+  return (
+    <>
+      <JSXRules />
+    </>
+  )
+}
+
+export default App
---
+++ Assignment-5/src/Components/JSXRules.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const JSXRules = () => {
+    return (
+        <>
+            <h1>JSX Rules</h1>
+            <ol>
+                <li>JSX must return a single parent element.</li>
+                <li>JSX elements must be properly closed.</li>
+                <li>JSX attributes use camelCase (e.g., className).</li>
+
+            </ol>
+        </>
+    )
+}
+
+export default JSXRules
